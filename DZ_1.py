217f46b88ec232c48f432bfcfeb9079685fd6c20
blisscreationist <101796532+blisscreationist@users.noreply.github.com>
2024-07-25 01:39:29 +0300

    first code

---
+++ DZ_1.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Параметры нормального распределения
+mean = 0       # Среднее значение
+std_dev = 1    # Стандартное отклонение
+num_samples = 1000  # Количество образцов
+
+# Генерация случайных чисел, распределенных по нормальному распределению
+data = np.random.normal(mean, std_dev, num_samples)
+
+plt.hist(data, bins=30, edgecolor='black')
+
+plt.title('Гистограмма нормального распределения')
+plt.xlabel('Значение')
+plt.ylabel('Частота')
+
+# Показ гистограммы
+plt.show()
+mean = 0       # Среднее значение
+std_dev = 1    # Стандартное отклонение
+num_samples = 1000  # Количество образцов
+
+# Генерация случайных чисел, распределенных по нормальному распределению
+data = np.random.normal(mean, std_dev, num_samples)
+
+plt.hist(data, bins=30, edgecolor='black')
+
+plt.title('Гистограмма нормального распределения')
+plt.xlabel('Значение')
+plt.ylabel('Частота')
+
+plt.show()
